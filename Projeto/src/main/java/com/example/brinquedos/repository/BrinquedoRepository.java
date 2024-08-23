e3405cd36be1a6345596a9427645e549f5445574
Giovanni Sguizzardi Conde <125572342+GiovanniSguizzardi@users.noreply.github.com>
2024-08-23 15:07:26 -0300

    Delete Projeto directory

--- Projeto/src/main/java/com/example/brinquedos/repository/BrinquedoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.brinquedos.repository;
-
-import com.example.brinquedos.model.Brinquedo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BrinquedoRepository extends JpaRepository<Brinquedo, Long> {
-}
